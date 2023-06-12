cb839c791abaaaddbef90f4d0f574b18c1612753
David Estephanian <desteph@protonmail.com>
2023-06-12 12:18:47 -0700

    Added lexer data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::CharIndices;
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    Select,
+    From,
+    Where,
+    And,
+    As,
+    In,
+    Dot,
+    Equal,
+    Insert,
+    Into,
+    Values,
+    Inner,
+    Left,
+    Right,
+    Join,
+    On,
+    LParen,
+    RParen,
+    Create,
+    Database,
+    Table,
+    Identifier(String),
+    Literal(String),
+    StringLiteral(String),
+    Unknown(char),
 }
+
+enum State {
+    Start,
+    Identifier,
+    Literal,
+    StringLiteral,
+}
+
+struct Lexer<'a> {
+    input: &'a str,
+    chars: CharIndices<'a>,
+    current_char: Option<(usize, char)>,
+    state: State,
+}
+
+fn main() {}
