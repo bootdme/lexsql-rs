b3fae16db04e63eea5e6ec094b7d4beef7dd5a75
David Estephanian <desteph@protonmail.com>
2023-06-12 13:40:30 -0700

    Function to get current substring

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,6 @@ enum Token {
     Unknown(char),
 }
 
-#[derive(Debug)]
 enum State {
     Start,
     Identifier,
@@ -37,7 +36,6 @@ enum State {
     StringLiteral,
 }
 
-#[derive(Debug)]
 struct Lexer<'a> {
     input: &'a str,
     chars: CharIndices<'a>,
@@ -60,11 +58,14 @@ impl<'a> Lexer<'a> {
     fn bump(&mut self) {
         self.current_char = self.chars.next();
     }
+
+    fn current_substring(&self, start: usize) -> &'a str {
+        let end = self.current_char.as_ref().map(|(i, _)| *i).unwrap_or(start);
+        &self.input[start..end]
+    }
 }
 
 fn main() {
-    let input = "test";
+    let input = r#"SELECT o.OrderID, o.OrderDate, c.CustomerName FROM Customers AS c, Orders AS o WHERE c.CustomerName="Around the Horn" AND c.CustomerID=o.CustomerID;"#;
     let mut lexer = Lexer::new(input);
-
-    println!("{:?}", lexer);
 }
